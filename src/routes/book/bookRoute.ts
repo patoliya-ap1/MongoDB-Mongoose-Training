c85de8efe41f8c59a098c4f2a35078df4641b73e
patoliya-ap1 <patoliya.ap1@gmail.com>
2026-03-19 18:21:39 +0530

    ref user-and-route-controller

--- src/routes/book/bookRoute.ts
+++ src/routes/book/bookRoute.ts
@@ -1,56 +1,17 @@
 import express from "express";
-import { BookModel } from "../../models/book.model";
-import { FilterForBooks } from "../../utility/types";
+import {
+  createBook,
+  getBooks,
+  getBooksAveragePerCategory,
+} from "../../controller/book/bookController";
 
 export const bookRouter = express.Router();
 
 // create books
-
-bookRouter.post("/", async (req, res) => {
-  const bookData = req.body;
-
-  const newBook = new BookModel(bookData);
-  const savedBook = await newBook.save();
-
-  res.status(201).json({
-    success: "true",
-    message: "new book created successfully",
-    newBook: savedBook,
-  });
-});
+bookRouter.post("/", createBook);
 
 // get books and find books by category
-
-bookRouter.get("/", async (req, res) => {
-  const category = req.query.category as string;
-
-  const filterObj: FilterForBooks = {};
-
-  if (category) {
-    filterObj.category = category;
-  }
-  const books = await BookModel.find(filterObj);
-
-  res.status(201).json({
-    success: "true",
-    message: "books fetched successfully",
-    books,
-  });
-});
+bookRouter.get("/", getBooks);
 
 //  books average price per category
-
-bookRouter.get("/categories/average-price", async (req, res) => {
-  const books = await BookModel.aggregate([
-    { $group: { _id: "$category", averagePrice: { $avg: "$price" } } },
-    { $project: { category: "$_id", _id: 0, averagePrice: 1 } },
-  ]);
-
-  res.status(201).json({
-    success: "true",
-    message: "books fetched successfully",
-    books,
-  });
-});
-
-
+bookRouter.get("/categories/average-price", getBooksAveragePerCategory);
--- src/routes/user/userRoute.ts
+++ src/routes/user/userRoute.ts
@@ -1,40 +1,10 @@
 import express from "express";
-import { UserModel } from "../../models/user.model";
-import { FilterForUsers } from "../../utility/types";
+import { createUsers, getUsers } from "../../controller/user/userController";
 
 export const userRouter = express.Router();
 
 // create books users
-
-userRouter.post("/", async (req, res) => {
-  const userData = req.body;
-
-  const newUser = new UserModel(userData);
-  const savedUser = await newUser.save();
-
-  res.status(201).json({
-    success: "true",
-    message: "new user created successfully",
-    newUser: savedUser,
-  });
-});
+userRouter.post("/", createUsers);
 
 //get users
-
-userRouter.get("/", async (req, res) => {
-  const userEmail = req.query.email as string;
-
-  const filterObj: FilterForUsers = {};
-
-  if (userEmail) {
-    filterObj.email = userEmail;
-  }
-
-  const users = await UserModel.find(filterObj).populate("borrowedBooks");
-
-  res.status(200).json({
-    success: true,
-    message: "books users fetched successfully.",
-    users,
-  });
-});
+userRouter.get("/", getUsers);
