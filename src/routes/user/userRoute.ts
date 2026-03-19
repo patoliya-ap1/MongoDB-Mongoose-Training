c23df55647b63416c5c94b56feee665f0e6fcfa7
patoliya-ap1 <patoliya.ap1@gmail.com>
2026-03-19 14:20:37 +0530

    library-management-system

---
+++ src/routes/user/userRoute.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import { UserModel } from "../../models/User";
+import { FilterForUsers } from "../../utility/types";
+
+export const userRouter = express.Router();
+
+// create books users
+
+userRouter.post("/", async (req, res) => {
+  const userData = req.body;
+
+  const newUser = new UserModel(userData);
+  const savedUser = await newUser.save();
+
+  res.status(201).json({
+    success: "true",
+    message: "new user created successfully",
+    newUser: savedUser,
+  });
+});
+
+//get users
+
+userRouter.get("/", async (req, res) => {
+  const userEmail = req.query.email as string;
+
+  const filterObj: FilterForUsers = {};
+
+  if (userEmail) {
+    filterObj.email = userEmail;
+  }
+
+  const users = await UserModel.find(filterObj).populate("borrowedBooks");
+
+  res.status(200).json({
+    success: true,
+    message: "books users fetched successfully.",
+    users,
+  });
+});
--- src/utility/types.ts
+++ src/utility/types.ts
@@ -1,3 +1,11 @@
 export interface CourseQuery {
   course?: string;
 }
+
+export interface FilterForUsers {
+  email?: string;
+}
+
+export interface FilterForBooks {
+  category?: string;
+}
